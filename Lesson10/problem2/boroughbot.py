8052e83a321b04e8a254a7508ff4d65df772172c
emilieaviles <43994902+emilieaviles@users.noreply.github.com>
2018-12-18 10:08:08 -0500

    1

---
+++ Lesson10/problem2/boroughbot.py
@@ -0,0 +1,19 @@
+print('-' * 65)
+
+print('Best Borough Bot:')
+
+print()
+
+borough = input('What borough are you from? ')
+
+if borough == 'Brooklyn':
+	print('Nice, Brooklyn is the best borough!')
+
+else:
+	print('Too bad, Brooklyn is the best!')
+	print('Obviously.')
+	print('But really Queens is the best.') 
+	print('Cary just thinks Brooklyn is the best since he thinks his opinion matters.')
+	print('His opinion actually matters, just not when it comes to boroughs.')
+
+print('-'*65)
---
+++ Lesson10/problem5/boozebot.py
@@ -0,0 +1,23 @@
+print('-' * 50)
+
+print('Booze Bot')
+
+print()
+
+print("I'm a vending machine that dispenses different kinds of alcohol. Yay technology. Please answer the following question:")
+
+print()
+
+age = input('How old are you? ')
+
+age = int(age)
+
+if age >= 21: 
+	order = input('What would you like to order? ')
+	print('Alrighty, cool guy. Please pay by swiping card on that flashing thing to the right. Pick up your ' + order + ' from the slot below.')
+
+else: 
+	years = 21 - age
+	years = str(years)
+	print("I think you're a bit too young to have alcohol. Come back in " + years + " years to order.")
+print('-' * 50)
